d4cb77b469204c2245697917b236c23e0f829189
Gabriel Martins Padoin <gabrielmpwork@gmail.com>
2020-05-10 02:22:08 -0300

    Componente com Propriedade

--- src/index.js
+++ src/index.js
@@ -1,7 +1,9 @@
 import './index.css'
 import ReactDOM from 'react-dom'
 import React from 'react'
+
 import Primeiro from './components/basicos/Primeiro'
+import ComParametro from './components/basicos/ComParametro'
 
 const root = document.getElementById('root')
 // const tag = <strong>Olá, Mundo!</strong>
@@ -11,6 +13,7 @@ ReactDOM.render(
     <div>
         {/* { tag } */}
         <Primeiro/>
+        <ComParametro titulo="Top das balada" subtitulo="Rolezêra"/>
     </div>,
     root
 )
