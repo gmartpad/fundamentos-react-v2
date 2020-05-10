94fdfd8cd1bc398a8bb668f9a9136ca6e9533163
Gabriel Martins Padoin <gabrielmpwork@gmail.com>
2020-05-10 11:32:18 -0300

    React Fragment

--- src/index.js
+++ src/index.js
@@ -4,16 +4,20 @@ import React from 'react'
 
 import Primeiro from './components/basicos/Primeiro'
 import ComParametro from './components/basicos/ComParametro'
+import Fragmento from './components/basicos/Fragmento'
 
 const root = document.getElementById('root')
 // const tag = <strong>Olá, Mundo!</strong>
 
 
 ReactDOM.render(
-    <div>
+    <React.Fragment>
         {/* { tag } */}
         <Primeiro/>
-        <ComParametro titulo="Top das balada" subtitulo="Rolezêra"/>
-    </div>,
+        <ComParametro 
+            titulo="Top das balada" 
+            subtitulo="Rolezêra"/>
+            <Fragmento/>
+    </React.Fragment>,
     root
 )
